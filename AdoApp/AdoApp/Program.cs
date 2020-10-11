0f3821d7a3596acf1ecddabf2d4db0a3fe1ae178
Ivan Saji Abraham <ivansaji619@gmail.com>
2020-10-11 11:42:28 +0530

    Connection Establish and Create Table

---
+++ AdoApp/AdoApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Import for using SQL CLient
+using System.Data.SqlClient;
+
+namespace AdoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calling Constructor so that automatic table creation occurs
+            new Program().CreateTable();
+        }
+
+        //method for creating Table
+        public void CreateTable()
+        {
+            SqlConnection con = null;
+            try
+            {
+                //creating Connection
+                con = new SqlConnection("data source=.; database=Student; integrated security = SSPI");
+                //writing sql query
+                SqlCommand cm = new SqlCommand("create table student(id not null,name varchar(100), email varchar(50), join_date date)", con);
+                //Opening Connection
+                con.Open();
+                //Executing the Query
+                cm.ExecuteNonQuery();
+                //Displaying a message
+                Console.WriteLine("Table Created Successfully");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Table Creation failed    " + e);
+            }
+            //closing Connection
+            finally
+            {
+                con.Close();
+            }
+        }
+    }
+}
