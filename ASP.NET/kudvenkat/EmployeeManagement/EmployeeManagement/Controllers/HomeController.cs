3c849f76327595f38d476ac4dcbaaccc40eb5bdf
Ivan Saji Abraham <ivansaji619@gmail.com>
2020-11-22 09:08:53 +0530

    Controllers learnt + models begun

--- ASP.NET/kudvenkat/EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
+++ ASP.NET/kudvenkat/EmployeeManagement/EmployeeManagement/Controllers/HomeController.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public JsonResult Index()
         {
-            return ("hello from HomeCOntroller MVC");
+            return Json(new { id=1, name = "Ivan" });
         }
     }
 }
---
+++ ASP.NET/kudvenkat/EmployeeManagement/EmployeeManagement/Models/MockEmployeeRespository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class MockEmployeeRespository : IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRespository()
+        {
+            _employeeList - new List<Employee>()
+            {
+                new Employee() {Id = 1, Name = "Mary" , Department = "HR", Email = "mary@gmail.com"};
+                new Employee() {Id = 1, Name = "Jane", Department = "IT", Email = "jane@gmail.com" };
+                new Employee() { Id = 1, Name = "Ivin", Department = "IT", Email = "ivin@gmail.com" };
+        }
+        }
+
+        public Employee GetEmployee(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
