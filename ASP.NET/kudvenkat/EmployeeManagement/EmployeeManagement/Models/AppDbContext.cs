7ae7901abd0096fa1ca7f4e97c78ca2fc8ff5ea5
Ivan Saji Abraham <ivansaji619@gmail.com>
2020-12-12 15:13:21 +0530

    Entity Framework Begin - DB Context

---
+++ ASP.NET/kudvenkat/EmployeeManagement/EmployeeManagement/Models/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement.Models
+{
+    public class AppDbContext : DbContext
+    {
+        //For DB contextclass to perform any task it requires DBCOntext options clss (Connection string, Database provider etc)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
