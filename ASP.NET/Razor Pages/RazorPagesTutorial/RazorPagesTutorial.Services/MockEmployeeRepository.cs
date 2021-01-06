0448cffeead2020d4cceb20e27b93d1a50a14513
Ivan Saji Abraham <ivansaji619@gmail.com>
2021-01-06 20:10:07 +0530

    List Razor Pages

--- ASP.NET/Razor Pages/RazorPagesTutorial/RazorPagesTutorial.Services/MockEmployeeRepository.cs
+++ ASP.NET/Razor Pages/RazorPagesTutorial/RazorPagesTutorial.Services/MockEmployeeRepository.cs
@@ -13,7 +13,7 @@ namespace RazorPagesTutorial.Services
             _employeeList = new List<Employee>()
             {
                 new Employee() {Id = 1, Name = "Mary", Department=Dept.HR, Email="mary@pragimtech.com" , PhotoPath="mary.png" },
-                new Employee() {Id = 2, Name = "John", Department=Dept.IT, Email="john@pragimtech.com" , PhotoPath="john.png" },
+                new Employee() {Id = 2, Name = "John", Department=Dept.IT, Email="john@pragimtech.com" },
                 new Employee() {Id = 3, Name = "Sara", Department=Dept.IT, Email="sara@pragimtech.com" , PhotoPath="sara.png" },
                 new Employee() {Id = 4, Name = "David", Department=Dept.Payroll, Email="david@pragimtech.com" , PhotoPath="david.png" },
             };
