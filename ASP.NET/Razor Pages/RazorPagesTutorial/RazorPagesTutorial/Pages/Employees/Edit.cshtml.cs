3e4233b577222b4dafce02d30eb689365ec736fa
Ivan Saji Abraham <ivansaji619@gmail.com>
2021-01-20 00:43:55 +0530

    File Upload

--- ASP.NET/Razor Pages/RazorPagesTutorial/RazorPagesTutorial/Pages/Employees/Edit.cshtml.cs
+++ ASP.NET/Razor Pages/RazorPagesTutorial/RazorPagesTutorial/Pages/Employees/Edit.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RazorPagesTutorial.Models;
@@ -12,12 +15,16 @@ namespace RazorPagesTutorial.Pages.Employees
     public class EditModel : PageModel
     {
         private readonly IEmployeeRepository employeeRepository;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
-        public EditModel(IEmployeeRepository employeeRepository)
+        public EditModel(IEmployeeRepository employeeRepository, IWebHostEnvironment webHostEnvironment)
         {
             this.employeeRepository = employeeRepository;
+            this.webHostEnvironment = webHostEnvironment;
         }
 
+        [BindProperty]
+        public IFormFile Photo { get; set; }
         public Employee Employee { get; private set; }
 
         public IActionResult OnGet(int id)
@@ -33,8 +40,35 @@ namespace RazorPagesTutorial.Pages.Employees
 
         public IActionResult OnPost(Employee employee)
         {
+            if(Photo != null)
+            {
+                if(employee.PhotoPath != null)
+                {
+                    string filePath = Path.Combine(webHostEnvironment.WebRootPath, "images", employee.PhotoPath);
+                    System.IO.File.Delete(filePath);
+                }
+                employee.PhotoPath = ProcessUploadedFile();
+            }
+
             Employee = employeeRepository.Update(employee);
             return RedirectToPage("Index");
         }
+
+        private string ProcessUploadedFile()
+        {
+            string uniqueFileName = null;
+            if(Photo != null)
+            {
+                string uploadsFolder = Path.Combine(webHostEnvironment.WebRootPath, "images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + Photo.FileName;
+                string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                using(var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    Photo.CopyTo(fileStream);
+                }
+            }
+            return uniqueFileName;
+        }
+
     }
 }
